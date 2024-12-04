276e5942ae68f0e00a6232f6bbc3c1d6eadc9950
AbhishekPatwal <abhishekpatwalwina@gmail.com>
2024-12-04 11:48:26 +0530

    api Cross Origin

--- signUp/src/main/java/com/example/signUp/Controller/SignUp.java
+++ signUp/src/main/java/com/example/signUp/Controller/SignUp.java
@@ -1,9 +1,8 @@
 package com.example.signUp.Controller;
 
 import com.example.signUp.DTO.UserDTO;
-import com.example.signUp.Service.SignUpService;
+import com.example.signUp.Service.Iservice;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 public class SignUp {
 
     @Autowired
-    public SignUpService service;
+    public Iservice service;
 
     @PostMapping("/signUp")
     public ResponseEntity<?> signUp(@RequestBody UserDTO userDTO)
